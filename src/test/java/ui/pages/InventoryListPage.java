8a5bedbac8b0df101f2a982b62cb107f3ea4f67c
Вадим Петров <vadimpetrov@MacBook-Air-Vadim.local>
2026-05-29 08:50:57 +0300

    refactor: improve InventoryListPage

--- src/test/java/ui/pages/InventoryListPage.java
+++ src/test/java/ui/pages/InventoryListPage.java
@@ -1,19 +1,21 @@
 package ui.pages;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.*;
 import io.qameta.allure.Step;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import ui.config.Config;
 import ui.helpers.CookieHelper;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.value;
-import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.*;
 
 public class InventoryListPage extends BasePage {
+
+    private final ElementsCollection inventoryItemCount =
+            $$(By.xpath("//div[@class='inventory_list']/div[@class='inventory_item']"));
+    private final SelenideElement azSortElement = $x("//*[@class='select_container']/select/option[@value='az']");
 
     public InventoryListPage() {
         URL += "/inventory.html";
@@ -27,13 +29,13 @@ public class InventoryListPage extends BasePage {
 
     @Step("Проверяем сортировку по умолчанию: Name (A to Z)")
     public InventoryListPage assertDefaultSortIsAZ() {
-        $(".product_sort_container").shouldHave(value("az"));
+        azSortElement.shouldHave(value("az"));
         return this;
     }
 
     @Step("Получаем количество элементов на странице")
     public InventoryListPage assertInventoryItemsCount(int num) {
-        $$(".inventory_item").shouldHave(size(num));
+        inventoryItemCount.shouldHave(size(num));
         return this;
     }
 }
