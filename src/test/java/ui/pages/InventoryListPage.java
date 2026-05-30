fc90ed1b599c1a9f8b1b5edfbc5d5f8527fcf16b
Вадим Петров <vadimpetrov@MacBook-Air-Vadim.local>
2026-05-30 11:47:53 +0300

    add dropmenu tets

--- src/test/java/ui/pages/InventoryListPage.java
+++ src/test/java/ui/pages/InventoryListPage.java
@@ -2,10 +2,15 @@ package ui.pages;
 
 import com.codeborne.selenide.*;
 import io.qameta.allure.Step;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Cookie;
+import org.junit.jupiter.api.Assertions;
 import ui.config.Config;
 import ui.helpers.CookieHelper;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.value;
@@ -13,9 +18,17 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class InventoryListPage extends BasePage {
 
+    private final SelenideElement inventoryList =
+            $x("//div[@class='inventory_list']");
     private final ElementsCollection inventoryItemCount =
-            $$(By.xpath("//div[@class='inventory_list']/div[@class='inventory_item']"));
-    private final SelenideElement azSortElement = $x("//*[@class='select_container']/select/option[@value='az']");
+            inventoryList.$$x(".//div[@class='inventory_item']");
+    private final ElementsCollection inventoryItemPrices =
+            inventoryList.$$x(".//div[@data-test='inventory-item-price']");
+
+    private final SelenideElement sortDropdown = $x("//*[@data-test='product-sort-container']");
+    private final SelenideElement azSortElement =
+            sortDropdown.$x("./option[@value='az']");
+
 
     public InventoryListPage() {
         URL += "/inventory.html";
@@ -36,6 +49,35 @@ public class InventoryListPage extends BasePage {
     @Step("Получаем количество элементов на странице")
     public InventoryListPage assertInventoryItemsCount(int num) {
         inventoryItemCount.shouldHave(size(num));
+        return this;
+    }
+
+    @Step("Выбрать сортировку A→Z")
+    public InventoryListPage selectSort(String value) {
+        sortDropdown.selectOptionByValue(value);
+        return this;
+    }
+
+    @Step("Проверяем сортировку товаров")
+    public InventoryListPage assertNamesSorted(Comparator<String> comparator) {
+        List<String> actual = inventoryItemCount.texts();
+        List<String> sorted = actual.stream()
+                .sorted(comparator)
+                .collect(Collectors.toList());
+        Assertions.assertEquals(sorted, actual, "Товары отсортированы неверно");
+        return this;
+    }
+
+    @Step("Проверяем сортировку товаров")
+    public InventoryListPage assertPricesSorted(Comparator<Double> comparator) {
+        List<Double> actual = inventoryItemPrices.texts()
+                .stream()
+                .map(price -> Double.parseDouble(price.replace("$", "")))
+                .collect(Collectors.toList());
+        List<Double> sorted = actual.stream()
+                .sorted(comparator)
+                .collect(Collectors.toList());
+        Assertions.assertEquals(sorted, actual, "Товары отсортированы неверно");
         return this;
     }
 }
--- src/test/java/ui/tests/mainPage/InventoryPagesTest.java
+++ src/test/java/ui/tests/mainPage/InventoryPagesTest.java
@@ -3,11 +3,31 @@ package ui.tests.mainPage;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import ui.config.Config;
 import ui.pages.Pages;
 import ui.settings.BaseSettings;
 
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 public class InventoryPagesTest extends BaseSettings {
+
+    static Stream<Arguments> sortProvider() {
+        return Stream.of(
+                Arguments.of(Config.sortAZ, Comparator.naturalOrder()),
+                Arguments.of(Config.sortZA, Comparator.reverseOrder())
+        );
+    }
+
+    static Stream<Arguments> priceSortProvider() {
+        return Stream.of(
+                Arguments.of(Config.sortLohi, Comparator.<Double>naturalOrder()),
+                Arguments.of(Config.sortHilo, Comparator.<Double>reverseOrder())
+        );
+    }
 
     @BeforeEach
     public void authorize() {
@@ -16,8 +36,27 @@ public class InventoryPagesTest extends BaseSettings {
 
     @Test
     @Story("Сортировка по умолчанию")
-    public void getDefaultSort() {
+    public void assertDefaultSort() {
         Pages.inventoryList
-                .assertDefaultSortIsAZ();
+                .assertDefaultSortIsAZ()
+                .assertNamesSorted(Comparator.naturalOrder());
+    }
+
+    @ParameterizedTest
+    @MethodSource("sortProvider")
+    @Story("Сортировка товаров по названию")
+    public void sortByName(String sortValue, Comparator<String> comparator) {
+        Pages.inventoryList
+                .selectSort(sortValue)
+                .assertNamesSorted(comparator);
+    }
+
+    @ParameterizedTest
+    @MethodSource("priceSortProvider")
+    @Story("Сортировка товаров по цене")
+    public void sortByPrice(String sortValue, Comparator<Double> comparator) {
+        Pages.inventoryList
+                .selectSort(sortValue)
+                .assertPricesSorted(comparator);
     }
 }
