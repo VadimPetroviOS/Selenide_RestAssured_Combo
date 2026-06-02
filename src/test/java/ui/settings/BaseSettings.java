fec2e4a5a7ed47cf4a42a847ba8e6fe69d32e159
Вадим Петров <vadimpetrov@MacBook-Air-Vadim.local>
2026-06-02 12:12:43 +0300

    fix: headless chrome for CI

--- src/test/java/ui/settings/BaseSettings.java
+++ src/test/java/ui/settings/BaseSettings.java
@@ -6,13 +6,18 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseSettings {
     @BeforeEach
     void setUp() {
         Configuration.browser = "chrome";
-//        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
         SelenideLogger.addListener("allure", new AllureSelenide());
+
+        // для CI
+        Configuration.browserCapabilities = new ChromeOptions()
+                .addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
     }
 
     @AfterEach
